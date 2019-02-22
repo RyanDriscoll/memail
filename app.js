de5515ad378741e75e037bc855abf0c11abee649
ryan <driscollrp@gmail.com>
2019-02-22 10:04:55 -0700

    updated sendMail function to use oauth2 to authenticate the google email account

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express();
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8080;
+const { google } = require('googleapis');
+const {
+  EMAIL,
+  REFRESH_TOKEN,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL,
+} = process.env;
+const OAuth2 = google.auth.OAuth2;
 
 app.use(bodyParser.json());
 
@@ -11,40 +19,44 @@ app.use('/send', router);
 
 router.use('/', sendEmail);
 
-function sendEmail(req, res, next) {
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS
-    }
+async function sendEmail(req, res, next) {
+  const oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+  oauth2Client.setCredentials({
+    REFRESH_TOKEN,
   });
+  oauth2Client.getAccessToken().then(response => {
+    const accessToken = response.token;
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: EMAIL,
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        accessToken: accessToken,
+      },
+    });
 
-  var mailOptions = {
-    from: `MEmail <${process.env.EMAIL}>`,
-    to: req.body.email,
-    subject: req.body.title,
-    text: req.body.url
-  };
+    const mailOptions = {
+      from: `MEmail <${EMAIL}>`,
+      to: req.body.email,
+      subject: req.body.title,
+      text: req.body.url,
+    };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      res.json({
-        status: 'error'
-      });
-    } else {
-      res.json({
-        status: 'success'
-      });
-    }
+    transporter.sendMail(mailOptions, (error, response) => {
+      error ? console.log(error) : console.log(response);
+      transporter.close();
+    });
   });
 }
 
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   console.error(err, err.stack);
   res.status(err.status || 500).send(err);
 });
 
-app.listen(PORT, function () {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, function() {
   console.log(`Server is listening on port ${PORT}`);
 });
